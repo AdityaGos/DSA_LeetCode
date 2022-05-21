3d6c48365da527a1aea0274222b33f06e5cefd43
Aditya_G <59224309+AdityaGos@users.noreply.github.com>
2022-05-22 00:14:09 +0530

    Time: 51 ms (10.56%), Space: 16.9 MB (53.53%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         int ans=0;
-        set<int> st;
         for(int i=0;i<nums.size();i++)
         {
-            if(st.count(nums[i])){ st.erase(nums[i]);}
-            else st.insert(nums[i]);
-        }
-        for(auto x:st)
-        {
-            ans=x;
+            ans^=nums[i];
         }
         return ans;
     }
